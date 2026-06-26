c2682710807b82fd906b3715d9fafca2f019fb61
shibachuk <demchukar@gmail.com>
2026-06-26 15:48:09 +0300

    fifth attepmtion to edit file properly

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(join(__dirname, 'dist')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
